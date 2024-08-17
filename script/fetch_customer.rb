236a59b38d20ce28eb510b046c16decbb303d5a8
MexpressoBean <kingk102@gmail.com>
2024-08-17 17:14:39 -0500

    add comments about next steps

--- script/fetch_customer.rb
+++ script/fetch_customer.rb
@@ -26,3 +26,16 @@ jobber_data["data"]["clients"]["nodes"].each do |customer|
 end
 puts "TOTAL CUSTOMERS: #{jobber_data["data"]["clients"]["totalCount"]}"
 # puts jobber_data
+
+#######################
+# Both airtable and jobber api integrations are
+# working to pull data from my accounts!
+
+# Next step is to further automate grabbing the access code 
+# from the browser when we authenticate through jobber.
+# Currently there is a manual step of visiting the auth url 
+# and grabbing the code from the query params.
+
+# After that, we can begin the logic for the sync between jobber and airtable.
+# Start by syncing customers first.  Then branch out from there.
+#######################
