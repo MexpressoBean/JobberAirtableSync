cbe688dc98d13e684baef0bfb30347176f205e19
MexpressoBean <kingk102@gmail.com>
2024-08-18 20:15:13 -0500

    fix linter in main fetch customers script

--- script/fetch_customer.rb
+++ script/fetch_customer.rb
@@ -20,20 +20,20 @@ access_token = auth_manager.ensure_access_token
 jobber_client = Jobber::Client.new(access_token)
 jobber_data = jobber_client.fetch_customers
 
-puts "Jobber Customers:"
-jobber_data["data"]["clients"]["nodes"].each do |customer|
+puts 'Jobber Customers:'
+jobber_data['data']['clients']['nodes'].each do |customer|
   puts "ID: #{customer['id']}, Name: #{customer['firstName']} #{customer['lastName']}"
 end
-puts "TOTAL CUSTOMERS: #{jobber_data["data"]["clients"]["totalCount"]}"
+puts "TOTAL CUSTOMERS: #{jobber_data['data']['clients']['totalCount']}"
 # puts jobber_data
 
 #######################
 # Both airtable and jobber api integrations are
 # working to pull data from my accounts!
 
-# Next step is to further automate grabbing the access code 
+# Next step is to further automate grabbing the access code
 # from the browser when we authenticate through jobber.
-# Currently there is a manual step of visiting the auth url 
+# Currently there is a manual step of visiting the auth url
 # and grabbing the code from the query params.
 
 # After that, we can begin the logic for the sync between jobber and airtable.
